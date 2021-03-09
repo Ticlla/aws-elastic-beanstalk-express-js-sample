cecaea5783f6e80778a9bbcd6c8ef10ae46f29d6
Alcides Ticlla <bandycoot_17@hotmail.com>
2021-03-08 21:04:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! con el new pipeline'));
+app.get('/', (req, res) => res.send('this a pipeline test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
